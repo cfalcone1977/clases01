faa9dfd7bf23591f15096a17cba8731a3cfba1b5
Cristian Falcone <cristianfalcone@hotmail.com>
2025-04-13 22:57:58 -0300

    arrancando con clases lo basico

---
+++ index.ts
@@ -0,0 +1,90 @@
+class Televisor {
+    private brand: string;
+    private volume: number = 0;
+    private channel: number = 1;
+    private isOn: boolean = false;
+    private isMuted : boolean = false; // Si el volumen llega a 0 y damos al boton de bajar volumen
+    public constructor(brand: string) {
+        this.brand = brand;
+    }
+    public switchPower(): void {
+        this.isOn = !this.isOn
+    }
+
+    public muteOnOff(): void {
+        this.isMuted = !this.isMuted
+    }
+
+    private getTime(): string {
+        const fullDate = new Date()
+        return `${fullDate.getHours()}:${fullDate.getMinutes()}`
+
+
+    }
+    public info(): void {
+        if (this.isOn === false) {
+            return
+        } else {
+            console.log(`
+                Channel: ${this.channel};
+                Volume: ${this.volume};
+                Hour: ${this.getTime()};
+                Mute: ${this.isMuted};
+                -----------\n`)
+        }
+    }
+    public channelUp(): void {
+        if (this.channel === 99) {
+            this.channel = 1
+        } else {
+            this.channel += 1
+        }
+    }
+    public channelDown(): void {
+        if (this.channel === 1) {
+            this.channel = 99
+        } else {
+            this.channel -= 1
+        }
+    }
+    public selectChannel(channelNumber: number) {
+        if (channelNumber > 0 && channelNumber <= 99) {
+            this.channel = channelNumber
+        }
+    }
+
+    public volumeUp(): void {
+        if (this.isMuted){
+                          this.isMuted=false;
+                         } else {
+                        if (this.volume < 100) {
+                              this.volume = this.volume + 1;
+                                               }
+                                }
+    }
+    public volumeDown(): void {
+        if (this.isMuted){
+            this.isMuted=false;
+           } else {
+          if (this.volume > 0) {
+                this.volume = this.volume - 1;
+                                 }
+                  }     
+
+        }
+    }
+
+const TV1 = new Televisor("TLC");   
+TV1.switchPower(); 
+TV1.info();
+TV1.volumeUp();
+TV1.volumeUp();
+TV1.muteOnOff();
+TV1.volumeUp();
+TV1.volumeDown();
+TV1.volumeDown();
+TV1.volumeDown();
+TV1.volumeDown();
+TV1.volumeUp();
+TV1.info();
+
